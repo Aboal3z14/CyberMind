0bec5c80472ac1a62bcc50b31c334f0cbd546ac8
Aboal3z14 <155063399+Aboal3z14@users.noreply.github.com>
2025-09-10 10:05:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // ============================================
 let generatedOTP = null;
 let offsetX = 0, offsetY = 0, isDragging = false;
+
 // ============================================
 // 🚀 STARTUP & DOM INIT
 // ============================================
@@ -615,6 +616,7 @@ function applyTheme(theme) {
     document.body.style.backgroundColor = "#0b0b0d";
   }
 }
+
 
 
 
