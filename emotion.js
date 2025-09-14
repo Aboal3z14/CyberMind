370a9826b2b131d443130615d8990ec42f2ebc37
Aboal3z14 <155063399+Aboal3z14@users.noreply.github.com>
2025-09-14 19:19:04 +0300

    Update emotion.js

--- emotion.js
+++ emotion.js
@@ -3,3 +3,13 @@ async function loadModels() {
   await faceapi.nets.faceExpressionNet.loadFromUri('/models');
   console.log("✅ Models loaded");
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Load models
+  await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+  await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+  console.log("✅ Models loaded!");
+
+  // Start webcam
+  await startWebcam();
+});
