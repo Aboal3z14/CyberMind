6017a3c0658171bee0454d1bf844093fcbf79c0e
Aboal3z14 <155063399+Aboal3z14@users.noreply.github.com>
2025-09-15 09:01:45 +0300

    Update emotion.js

--- emotion.js
+++ emotion.js
@@ -76,7 +76,7 @@ async function startWebcam() {
 // ============================================
 async function runDetectionLoop(video) {
   const tinyOptions = new faceapi.TinyFaceDetectorOptions({
-    inputSize: 396,
+    inputSize: 320,
     scoreThreshold: 0.3,
   });
 
