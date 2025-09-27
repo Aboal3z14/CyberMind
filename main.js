045bf3d5f0022ebe37bb67930bc7c3c1fd948aa0
Aboal3z14 <155063399+Aboal3z14@users.noreply.github.com>
2025-09-27 15:29:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -309,7 +309,7 @@ function startGame() {
 }
 function startLevel() {
   document.getElementById("menu-screen").classList.add("hidden");
-  document.getElementById("level1-screen").classList.remove("hidden");
+  document.getElementById("level11-screen").classList.remove("hidden");
 }
 function showBadges() {
   openOverlay("badgesOverlay");
@@ -600,4 +600,3 @@ function applyTheme(theme) {
     document.body.style.backgroundColor = "#0b0b0d";
   }
 }
-
